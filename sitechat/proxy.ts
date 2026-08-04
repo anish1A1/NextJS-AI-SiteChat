f99d54f6eba8c1fa10ddb1ca91d5e24452c9b948
Anish Jaiswal <alloozpat87@gmail.com>
2026-08-04 16:42:25 +0545

    completed the proxy logic for roomId page

--- sitechat/proxy.ts
+++ sitechat/proxy.ts
@@ -4,16 +4,18 @@ import { NextRequest, NextResponse } from "next/server"
 import { redis } from "./lib/redis"
 import { nanoid } from "nanoid"
 
+// OverView check if the user is allowed to join room
+// If they are: Let them pass
+// If they are not: Send them back to Lobby/home
+
+
 export const proxy = async (req: NextRequest) => {
 
-    // OverView check if the user is allowed to join room
-    // If they are: Let them pass
-    // If they are not: Send them back to Lobby/home
     
     // This will give us the pathname of the current url.  
     const pathname = req.nextUrl.pathname
-    // this will check the rooms if 
-    // eg. localhost/room/joshroom
+    // this will check the rooms if  eg. localhost/room/joshroom
+    
     const roomMatch = pathname.match(/^\/room\/([^/]+)$/)
 
     if (!roomMatch) { 
@@ -24,6 +26,7 @@ export const proxy = async (req: NextRequest) => {
     // extracting the uri after room/{id} 
     const roomId = roomMatch[1]
 
+    // getting data from db.
     const meta = await redis.hgetall<{connected: string[]; createdAt:number}>(`meta:${roomId}`)
 
     // we added <> in hgetall to let typescript know the data and its type when receiving from redis db.
@@ -32,13 +35,40 @@ export const proxy = async (req: NextRequest) => {
         return NextResponse.redirect(new URL("/?error=room-not-found", req.url))
     }
 
+
+    /* 
+    We also need to check if the current user with token is already in the db. 
+    1. If YES, then :
+        do not readd him in the db.
+
+    2. If Room is full then:
+        Do not let him get on the chat page.
+
+    3. If Not then :
+        create a new token, set cookie and add the roomId with the new token for that user, and add all that in redis db.
+        Update the db connected list.
+    */
+   const existingToken = req.cookies.get("x-auth-token")?.value
+
+    // 1.    
+    if(existingToken && meta.connected.includes(existingToken)){
+        return NextResponse.next()
+    }
+
+    //2.
+    if (meta.connected.length >= 2) {
+        return NextResponse.redirect(new URL('/?error=room-full', req.url))
+    }
+
+
+    //3.
     const response = NextResponse.next()
     /* 
     The NextResponse.next() is used in middleware only. It allows the incoming requests to countinue routing normally to its intended destination. 
     Its used when we want to set cookies or modify response header.
     */
 
-    // generating 
+    // generating token
     const token = nanoid()
 
     // caching the rooms of that roomId.
@@ -51,6 +81,7 @@ export const proxy = async (req: NextRequest) => {
 
 
     // then we add the current roomId token of the connected user and set inside the connected list of redis db.
+
     // if another user comes in this url roomId then he will also be added in the room, ...meta.connceted keeps the current data (prev user roomId with token data) and also adds the current one users id in the list of redis db.
 
 
