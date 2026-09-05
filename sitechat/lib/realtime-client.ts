20d955208e770584f9a33500e799285c9fa65507
Anish Jaiswal <alloozpat87@gmail.com>
2026-09-05 18:41:25 +0545

    added description on how i added more codes

--- sitechat/lib/realtime-client.ts
+++ sitechat/lib/realtime-client.ts
@@ -6,3 +6,5 @@ import { createRealtime } from "@upstash/realtime/client"
 import type { RealtimeEvents } from "./realtime"
 
 export const {useRealtime} = createRealtime<RealtimeEvents>()
+
+// This useRealtime() Hook will be used for fetching data in realtime
